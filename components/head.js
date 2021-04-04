9e4ebce4dc3e5bd9d88ab316309206561b1ad983
Yamane <candoanything1031@gmail.com>
2021-04-04 20:14:52 +0900

    favicon追加

--- components/head.js
+++ components/head.js
@@ -41,6 +41,15 @@ export default function ({
       <meta name="twitter:description" content={description} />
       <meta name="twitter:image" content={image} />
       <link rel="canonical" href={url} />
+
+      <link rel="apple-touch-icon" sizes="180x180" href="/images/favicon_package/apple-touch-icon.png" />
+      <link rel="icon" type="image/png" sizes="32x32" href="/images/favicon_package/favicon-32x32.png" />
+      <link rel="icon" type="image/png" sizes="16x16" href="/images/favicon_package/favicon-16x16.png" />
+      <link rel="manifest" href="/images/favicon_package/site.webmanifest" />
+      <link rel="mask-icon" href="/images/favicon_package/safari-pinned-tab.svg" color="#5bbad5" />
+      <meta name="msapplication-TileColor" content="#da532c" />
+      <meta name="theme-color" content="#ffffff"></meta>
+      
       {/* <link rel="shortcut icon" href={'https://t-cr.jp/favicon.ico'} />
       <link rel="apple-touch-icon" href={'https://t-cr.jp/logo.png'} /> */}
     </Head>
