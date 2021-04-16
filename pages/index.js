0e192ab67d6afa1af516bff0cd33e307fe23a057
Yamane <candoanything1031@gmail.com>
2021-04-16 13:44:05 +0900

    react-div-100vhを試してみる

--- pages/index.js
+++ pages/index.js
@@ -7,6 +7,7 @@ import TalkMain from "components/talkMain";
 import styles from "styles/content.module.css";
 
 import { getSessionId } from "reqests/word";
+import Div100vh from 'react-div-100vh'
 
 export default function Home() {
   // 初期状態セット
@@ -19,10 +20,13 @@ export default function Home() {
     <div>
       <Head title = "会話" viewport = "width=420" />
 
-      <div className={styles.TalkBg}>
-        <Menu />
-        <TalkMain />
-      </div>
+      <Div100vh>
+        <div className={styles.TalkBg}>
+          <Menu />
+          <TalkMain />
+        </div>
+      </Div100vh>
+      
       {/* <Head viewport="width=device-width initial-scale=1.0" />
 
       <div className={styles.clickArea}>
