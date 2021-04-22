4cff0a0204962aae0b96a2e5b5f9cc148691dabd
Yamane <candoanything1031@gmail.com>
2021-04-22 17:44:11 +0900

    head修正

--- pages/_document.js
+++ pages/_document.js
@@ -7,6 +7,7 @@ export default class MyDocument extends Document {
     return (
       <Html>
         <Head>
+          <script data-ad-client="ca-pub-6473717830087020" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
           {/* Global Site Tag (gtag.js) - Google Analytics */}
           <script
             async
@@ -24,31 +25,6 @@ export default class MyDocument extends Document {
           `
             }}
           />
-          {/* <script
-            async
-            src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=${process.env.NEXT_PUBLIC_AD_ID}`}
-            crossorigin="anonymous"
-          ></script> */}
-          {/* <script data-ad-client="ca-pub-6473717830087020" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script> */}
-          <script
-            async
-            src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
-          />
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `
-               (adsbygoogle = window.adsbygoogle || []).push({
-                   google_ad_client: "ca-pub-6473717830087020",
-                   enable_page_level_ads: true
-              });
-                `
-            }}
-          />
-          {/* <script
-            data-ad-client={process.env.NEXT_PUBLIC_AD_ID}
-            async
-            src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
-          ></script> */}
         </Head>
         <body>
           <Main />
