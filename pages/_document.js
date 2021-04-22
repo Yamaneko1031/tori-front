e615f67320b51a5c3ca20b167e7657ca0cb63c1c
Yamane <candoanything1031@gmail.com>
2021-04-22 17:16:05 +0900

    テスト

--- pages/_document.js
+++ pages/_document.js
@@ -29,8 +29,26 @@ export default class MyDocument extends Document {
             src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=${process.env.NEXT_PUBLIC_AD_ID}`}
             crossorigin="anonymous"
           ></script> */}
-          <script data-ad-client={process.env.NEXT_PUBLIC_AD_ID} async
-            src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+          <script data-ad-client="ca-pub-6473717830087020" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+          {/* <script
+            async
+            src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+               (adsbygoogle = window.adsbygoogle || []).push({
+                   google_ad_client: "ca-pub-6473717830087020",
+                   enable_page_level_ads: true
+              });
+                `
+            }}
+          /> */}
+          {/* <script
+            data-ad-client={process.env.NEXT_PUBLIC_AD_ID}
+            async
+            src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
+          ></script> */}
         </Head>
         <body>
           <Main />
