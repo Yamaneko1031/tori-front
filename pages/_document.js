40bca7864304dcc87148467f40be6eb8d5f9f7c9
Yamane <candoanything1031@gmail.com>
2021-04-23 01:30:01 +0900

    head修正

--- pages/_document.js
+++ pages/_document.js
@@ -3,19 +3,19 @@ import Document, { Html, Head, Main, NextScript } from "next/document";
 import { GA_TRACKING_ID } from "../util/gtag";
 
 export default class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
+  }
+
   render() {
     return (
       <Html>
         <Head>
-          {/* <script
+          <script
             data-ad-client="ca-pub-6473717830087020"
             async
             src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
-          ></script> */}
-          <script
-            async
-            src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6473717830087020"
-            crossorigin="anonymous"
           ></script>
           {/* Global Site Tag (gtag.js) - Google Analytics */}
           <script
